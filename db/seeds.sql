27dae2e9a787c3637b59cd6fe7e23b9e89c6c0dd
Amber <amberrose9167@gmail.com>
2018-11-17 18:18:57 -0800

    final push

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Double Bacon Cheeseburger", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Sauted Mushroom Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Chili Cheese Burger", true);
